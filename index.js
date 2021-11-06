71627e8adeb25640a5cf182c614aa8a83abb3c6d
Wasanta <kwan_u_day@hotmail.com>
2021-11-06 11:55:02 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+// function writeCards(names, message){
+//     for (let name of names){
+//         console.log(`Thank you, ${name}, for the wonderful ${message} gift!`);
+//     }
+// }
+
+
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const arr = [];
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    arr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    //debugger;
+  }
+  return arr;
+}
+
+
+
+let num = 10;
+function countDown(num){
+    
+    while (num >= 0) {
+    console.log(num--);
+ }
+}
+
+countDown (num);
